fe54f49ff47f9bf1b672a2514150eb16541257ae
KAUAN LOPES <kauan.lopes@a.catolica-to.edu.br>
2024-04-25 20:54:07 -0300

    segundo commit

--- Main.py
+++ Main.py
@@ -1,1 +1,22 @@
-print("Hello Word")
+import requests
+
+API_KEY = "a6858788eb7fd83a4eb89064e87c52da"
+BASE_URL = "https://api.themoviedb.org/3"
+
+def buscar_filme_por_id(filme_id):
+    endpoint = f"{BASE_URL}/movie/{filme_id}"
+    params = {"api_key": API_KEY}
+    response = requests.get(endpoint, params=params)
+    if response.status_code == 200:
+        filme = response.json()
+        return filme
+    else:
+        print("Erro ao buscar filme:", response.status_code)
+        return None
+
+# Exemplo de uso:
+filme_id = 550  # ID do filme "Clube da Luta"
+filme_info = buscar_filme_por_id(filme_id)
+if filme_info:
+    print("Título:", filme_info["title"])
+    print("Sinopse:", filme_info["overview"])
