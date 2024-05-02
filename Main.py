b27a18be02fc86ee3f06e99e1385c84506a66da6
Winn4K <kauan99436601@gmail.com>
2024-05-02 13:02:48 -0300

    alterando index.html para home.html

--- Main.py
+++ Main.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template # type: ignore
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return render_template('index.html')
+def home():
+    return render_template('home.html')
 
 app.run(debug=True)
 
