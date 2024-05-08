8476f91954c462f1dd98886bf1f2771542b5a581
Winn4K <kauan99436601@gmail.com>
2024-05-08 17:56:49 -0300

    adicionando comentarios para auxiliar no entendimento do codigo

--- Main.py
+++ Main.py
@@ -5,13 +5,17 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+### Adicionando a chave da api e a url a duas variaveis, 
+### para ficar mais facil a reutilização no restante .py
 API_KEY = "a6858788eb7fd83a4eb89064e87c52da"
 BASE_URL = "https://api.themoviedb.org/3"
 
+### Rota para a página principal
 @app.route("/")
 def home():
     return render_template('home.html')
 
+### Rota para envio de formulario
 '''
 @app.route('/submit', methods=['GET', 'POST'])
 
@@ -43,7 +47,7 @@ def submit():
 
         return render_template('home.html', filme_return=filme_return)
 '''
-
+### Rota para atualização de autocomplete
 @app.route('/autocomplete', methods=['GET'])
 def autocomplete():
     query = request.args.get('q')
@@ -51,6 +55,8 @@ def autocomplete():
     results = response.json()['results']
     return jsonify([movie['title'] for movie in results[:5]])
 
+### Rota para a atualizar a imagem do banner após 
+### o usuario selecionar o filme e trocar de campo
 @app.route('/meu_form/<inputId>', methods=['POST', 'GET'])
 def meu_form(inputId):
     movie_id = None  # Inicialize movie_id com None
