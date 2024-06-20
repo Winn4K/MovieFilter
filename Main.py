ad2f313c7617d943777c246cd77c2ad81a20c412
Winn4K <kauan99436601@gmail.com>
2024-06-20 17:42:31 -0300

    Aplicação totalmente funcional!

--- Main.py
+++ Main.py
@@ -15,38 +15,6 @@ BASE_URL = "https://api.themoviedb.org/3"
 def home():
     return render_template('home.html')
 
-### Rota para envio de formulario
-"""@app.route('/submit', methods=['GET', 'POST'])
-
-def submit():
-
-    def buscar_filme_por_id(filme_id):
-        endpoint = f"{BASE_URL}/movie/{filme_id}"
-        params = {"api_key": API_KEY}
-        response = requests.get(endpoint, params=params)
-        if response.status_code == 200:
-            filme = response.json()
-            return filme
-        else:
-            print("Erro ao buscar filme:", response.status_code)
-            return None
-
-    def movieId():
-        # Aqui você pega o valor do input pelo nome do campo
-        if request.method == 'POST':
-            firstMovie = request.form.get('firstMovie')
-            return f'{firstMovie}'
-
-
-    # Exemplo de uso:
-    filme_id = movieId() 
-    filme_info = buscar_filme_por_id(filme_id)
-    if filme_info:
-        filme_return = filme_info["title"]
-
-        return render_template('home.html', filme_return=filme_return)"""
-
-
 ### Rota para atualização de autocomplete
 @app.route('/autocomplete', methods=['GET'])
 def autocomplete():
